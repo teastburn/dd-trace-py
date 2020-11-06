aeaf4cb9d4ed91755de6cea7bf9bc6d729d49173
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-11-05 22:45:25 -0500

    ci: use riot for profiling tests

--- riotfile.py
+++ riotfile.py
@@ -57,6 +57,51 @@ suites = [
         ],
     ),
     Suite(
+        name="profiling",
+        command="python -m tests.profiling.run pytest --capture=no --verbose tests/profiling/",
+        env=[
+            ("DD_PROFILE_TEST_GEVENT", lambda case: "1" if "gevent" in case.pkgs else None),
+        ],
+        cases=[
+            Case(
+                pys=[
+                    2.7,
+                    3.5,
+                    3.6,
+                    3.7,
+                    3.8,
+                    3.9,
+                ],
+                pkgs=[("gevent", [None, ""]), ("pytest-benchmark", [""])],
+            ),
+            # Min reqs tests
+            Case(
+                pys=[2.7],
+                pkgs=[
+                    ("gevent", ["==1.1.0"]),
+                    ("protobuf", ["==3.0.0"]),
+                    ("tenacity", ["==5.0.1"]),
+                    ("pytest-benchmark", [""]),
+                ],
+            ),
+            Case(
+                pys=[
+                    3.5,
+                    3.6,
+                    3.7,
+                    3.8,
+                    3.9,
+                ],
+                pkgs=[
+                    ("gevent", ["==1.4.0"]),
+                    ("protobuf", ["==3.0.0"]),
+                    ("tenacity", ["==5.0.1"]),
+                    ("pytest-benchmark", [""]),
+                ],
+            ),
+        ],
+    ),
+    Suite(
         name="pymongo",
         command="pytest tests/contrib/pymongo",
         cases=[
